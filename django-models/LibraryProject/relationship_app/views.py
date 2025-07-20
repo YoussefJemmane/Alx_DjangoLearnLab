550039255a33ef667112f9a000761eb07f2614fc
YoussefJemmane <yous.jemm@gmail.com>
2025-07-20 14:16:51 +0100

    test

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -44,17 +44,17 @@ def role_required(role):
     return user_passes_test(has_role)
 
 # Views for Admin
-@role_required('Admin')
+@user_passes_test(lambda u: hasattr(u, 'userprofile') and u.userprofile.role == 'Admin')
 def admin_view(request):
     return render(request, 'relationship_app/admin_view.html')
 
 # Views for Librarian
-@role_required('Librarian')
+@user_passes_test(lambda u: hasattr(u, 'userprofile') and u.userprofile.role == 'Librarian')
 def librarian_view(request):
     return render(request, 'relationship_app/librarian_view.html')
 
 # Views for Member
-@role_required('Member')
+@user_passes_test(lambda u: hasattr(u, 'userprofile') and u.userprofile.role == 'Member')
 def member_view(request):
     return render(request, 'relationship_app/member_view.html')
 
